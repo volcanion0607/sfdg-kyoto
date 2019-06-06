07c68b0802c6c08c4c3dcb14d3b019f38533d7e6
Tomoyuki Okada <okada2@infonic.co.jp>
2019-06-06 16:52:46 +0900

    Kyoto Salesforce Developers Meetup #1 Demo

---
+++ salesforce-csharp-soap-sample/MainForm.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace salesforce_csharp_soap_sample
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textUserName = new System.Windows.Forms.TextBox();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.labelUserName = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonGetContacts = new System.Windows.Forms.Button();
+            this.buttonLogout = new System.Windows.Forms.Button();
+            this.dataGridViewContacts = new System.Windows.Forms.DataGridView();
+            this.FirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Phone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewContacts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textUserName
+            // 
+            this.textUserName.Location = new System.Drawing.Point(92, 6);
+            this.textUserName.Name = "textUserName";
+            this.textUserName.Size = new System.Drawing.Size(213, 19);
+            this.textUserName.TabIndex = 0;
+            // 
+            // textPassword
+            // 
+            this.textPassword.Location = new System.Drawing.Point(92, 31);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.PasswordChar = '*';
+            this.textPassword.Size = new System.Drawing.Size(213, 19);
+            this.textPassword.TabIndex = 1;
+            // 
+            // labelUserName
+            // 
+            this.labelUserName.AutoSize = true;
+            this.labelUserName.Location = new System.Drawing.Point(12, 9);
+            this.labelUserName.Name = "labelUserName";
+            this.labelUserName.Size = new System.Drawing.Size(47, 12);
+            this.labelUserName.TabIndex = 2;
+            this.labelUserName.Text = "ユーザ名";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(12, 34);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(52, 12);
+            this.labelPassword.TabIndex = 3;
+            this.labelPassword.Text = "パスワード";
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(317, 29);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 4;
+            this.buttonLogin.Text = "ログイン";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.ButtonLogin_Click);
+            // 
+            // buttonGetContacts
+            // 
+            this.buttonGetContacts.Location = new System.Drawing.Point(14, 70);
+            this.buttonGetContacts.Name = "buttonGetContacts";
+            this.buttonGetContacts.Size = new System.Drawing.Size(138, 23);
+            this.buttonGetContacts.TabIndex = 5;
+            this.buttonGetContacts.Text = "取引先責任者を取得";
+            this.buttonGetContacts.UseVisualStyleBackColor = true;
+            this.buttonGetContacts.Click += new System.EventHandler(this.ButtonGetContacts_Click);
+            // 
+            // buttonLogout
+            // 
+            this.buttonLogout.Location = new System.Drawing.Point(398, 29);
+            this.buttonLogout.Name = "buttonLogout";
+            this.buttonLogout.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogout.TabIndex = 6;
+            this.buttonLogout.Text = "ログアウト";
+            this.buttonLogout.UseVisualStyleBackColor = true;
+            this.buttonLogout.Click += new System.EventHandler(this.ButtonLogout_Click);
+            // 
+            // dataGridViewContacts
+            // 
+            this.dataGridViewContacts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewContacts.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.FirstName,
+            this.LastName,
+            this.Email,
+            this.Phone});
+            this.dataGridViewContacts.Location = new System.Drawing.Point(14, 99);
+            this.dataGridViewContacts.Name = "dataGridViewContacts";
+            this.dataGridViewContacts.RowTemplate.Height = 21;
+            this.dataGridViewContacts.Size = new System.Drawing.Size(462, 339);
+            this.dataGridViewContacts.TabIndex = 7;
+            // 
+            // FirstName
+            // 
+            this.FirstName.DataPropertyName = "FirstName";
+            this.FirstName.HeaderText = "FirstName";
+            this.FirstName.Name = "FirstName";
+            // 
+            // LastName
+            // 
+            this.LastName.DataPropertyName = "LastName";
+            this.LastName.HeaderText = "LastName";
+            this.LastName.Name = "LastName";
+            // 
+            // Email
+            // 
+            this.Email.DataPropertyName = "Email";
+            this.Email.HeaderText = "Email";
+            this.Email.Name = "Email";
+            // 
+            // Phone
+            // 
+            this.Phone.DataPropertyName = "Phone";
+            this.Phone.HeaderText = "Phone";
+            this.Phone.Name = "Phone";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 450);
+            this.Controls.Add(this.dataGridViewContacts);
+            this.Controls.Add(this.buttonLogout);
+            this.Controls.Add(this.buttonGetContacts);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelUserName);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.textUserName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "sfdgkyoto";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewContacts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textUserName;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.Label labelUserName;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Button buttonGetContacts;
+        private System.Windows.Forms.Button buttonLogout;
+        private System.Windows.Forms.DataGridView dataGridViewContacts;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LastName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Phone;
+    }
+}
+
---
+++ salesforce-csharp-soap-sample/MainForm.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.ServiceModel;
+using salesforce_csharp_soap_sample.sforce;
+using System.Net;
+
+namespace salesforce_csharp_soap_sample
+{
+    public partial class MainForm : Form
+    {
+
+        private static SoapClient loginClient;
+        private static SoapClient client;
+        private static SessionHeader header;
+        private static EndpointAddress endpoint;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+            buttonLogin.Enabled = true;
+            buttonLogout.Enabled = false;
+            buttonGetContacts.Enabled = false;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void ButtonLogin_Click(object sender, EventArgs e)
+        {
+            if (login())
+            {
+                MessageBox.Show("Login Successful!", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                buttonLogin.Enabled = false;
+                buttonLogout.Enabled = true;
+                buttonGetContacts.Enabled = true;
+            }
+            else
+            {
+                buttonLogin.Enabled = true;
+                buttonLogout.Enabled = false;
+                buttonGetContacts.Enabled = false;
+            }
+        }
+
+        private void ButtonGetContacts_Click(object sender, EventArgs e)
+        {
+            getContacts();
+        }
+
+        private void ButtonLogout_Click(object sender, EventArgs e)
+        {
+            if (logout())
+            {
+                MessageBox.Show("Logged out.", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                buttonGetContacts.Enabled = false;
+                buttonLogout.Enabled = false;
+                buttonLogin.Enabled = true;
+            }
+            else
+            {
+                buttonGetContacts.Enabled = true;
+                buttonLogout.Enabled = true;
+                buttonLogin.Enabled = false;
+            }
+        }
+
+        private bool login()
+        {
+            string username = textUserName.Text;
+            string password = textPassword.Text;
+
+            loginClient = new SoapClient();
+            LoginResult loginResult;
+            try
+            {
+                loginResult = loginClient.login(null, username, password);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (loginResult.passwordExpired)
+            {
+                MessageBox.Show("パスワードの有効期限が切れています。", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            endpoint = new EndpointAddress(loginResult.serverUrl);
+
+            header = new SessionHeader();
+            header.sessionId = loginResult.sessionId;
+
+            client = new SoapClient("Soap", endpoint);
+
+            //showUserInfo(loginResult, loginResult.serverUrl);
+
+            return true;
+        }
+
+        private void showUserInfo(LoginResult lr, String authEP)
+        {
+            try
+            {
+                GetUserInfoResult userInfo = lr.userInfo;
+                String userId = userInfo.userId;
+                String userFullName = userInfo.userFullName;
+                String userEmail = userInfo.userEmail;
+                String serverUrl = lr.serverUrl;
+
+                String userInfoText = $@"UserID:
+{userId}
+User Full Name:
+{userFullName}
+User Email:
+{userEmail}
+Auth End Point:
+{authEP}
+Service End Point:
+{serverUrl}
+";
+                MessageBox.Show(userInfoText, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void getContacts()
+        {
+            String soqlQuery = "SELECT FirstName, LastName, Email, Phone FROM Contact";
+            try
+            {
+                QueryResult qr;
+                LimitInfo[] limitInfos =
+                    client.query(
+                    header,
+                    null,
+                    null,
+                    null,
+                    soqlQuery,
+                    out qr
+                    );
+
+                bool done = false;
+
+                if (qr.size > 0)
+                {
+                    List<MyContact> contacts = new List<MyContact>();
+                    while (!done)
+                    {
+                        sObject[] records = qr.records;
+                        for (int i = 0; i<records.Length; i++)
+                        {
+                            Contact contact = (Contact)records[i];
+                            MyContact myContact = new MyContact();
+                            myContact.FirstName = contact.FirstName;
+                            myContact.LastName = contact.LastName;
+                            myContact.Email = contact.Email;
+                            myContact.Phone = contact.Phone;
+                            contacts.Add(myContact);
+                        }
+
+                        if (qr.done)
+                        {
+                            done = true;
+                        }
+                        else
+                        {
+                            limitInfos =
+                                client.queryMore(
+                                header,
+                                null,
+                                qr.queryLocator,
+                                out qr
+                                );
+                        }
+                    }
+
+                    dataGridViewContacts.DataSource = contacts;
+
+                }
+                else
+                {
+                    MessageBox.Show("条件に一致するレコードが存在しません。", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool logout()
+        {
+            try
+            {
+                client.logout(header);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+
+    }
+
+    public class MyContact
+    {
+        public String FirstName
+        {
+            set; get;
+        }
+        public String LastName
+        {
+            set; get;
+        }
+        public String Email
+        {
+            set; get;
+        }
+        public String Phone
+        {
+            set; get;
+        }
+    }
+}
