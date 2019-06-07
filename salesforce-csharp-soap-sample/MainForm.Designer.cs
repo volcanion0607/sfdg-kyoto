750ec37d57ab140e54e5e9cd709bb400c451cc7e
Tomoyuki Okada <okada2@infonic.co.jp>
2019-06-07 18:10:30 +0900

    勉強会用にフォントサイズを大きくした。

--- salesforce-csharp-soap-sample/MainForm.Designer.cs
+++ salesforce-csharp-soap-sample/MainForm.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.textUserName = new System.Windows.Forms.TextBox();
             this.textPassword = new System.Windows.Forms.TextBox();
             this.labelUserName = new System.Windows.Forms.Label();
@@ -45,42 +47,47 @@
             // 
             // textUserName
             // 
-            this.textUserName.Location = new System.Drawing.Point(92, 6);
+            this.textUserName.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textUserName.Location = new System.Drawing.Point(149, 9);
             this.textUserName.Name = "textUserName";
-            this.textUserName.Size = new System.Drawing.Size(213, 19);
+            this.textUserName.Size = new System.Drawing.Size(643, 31);
             this.textUserName.TabIndex = 0;
             // 
             // textPassword
             // 
-            this.textPassword.Location = new System.Drawing.Point(92, 31);
+            this.textPassword.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textPassword.Location = new System.Drawing.Point(149, 46);
             this.textPassword.Name = "textPassword";
             this.textPassword.PasswordChar = '*';
-            this.textPassword.Size = new System.Drawing.Size(213, 19);
+            this.textPassword.Size = new System.Drawing.Size(643, 31);
             this.textPassword.TabIndex = 1;
             // 
             // labelUserName
             // 
             this.labelUserName.AutoSize = true;
-            this.labelUserName.Location = new System.Drawing.Point(12, 9);
+            this.labelUserName.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelUserName.Location = new System.Drawing.Point(12, 12);
             this.labelUserName.Name = "labelUserName";
-            this.labelUserName.Size = new System.Drawing.Size(47, 12);
+            this.labelUserName.Size = new System.Drawing.Size(94, 24);
             this.labelUserName.TabIndex = 2;
             this.labelUserName.Text = "ユーザ名";
             // 
             // labelPassword
             // 
             this.labelPassword.AutoSize = true;
-            this.labelPassword.Location = new System.Drawing.Point(12, 34);
+            this.labelPassword.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelPassword.Location = new System.Drawing.Point(12, 48);
             this.labelPassword.Name = "labelPassword";
-            this.labelPassword.Size = new System.Drawing.Size(52, 12);
+            this.labelPassword.Size = new System.Drawing.Size(103, 24);
             this.labelPassword.TabIndex = 3;
             this.labelPassword.Text = "パスワード";
             // 
             // buttonLogin
             // 
-            this.buttonLogin.Location = new System.Drawing.Point(317, 29);
+            this.buttonLogin.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonLogin.Location = new System.Drawing.Point(413, 106);
             this.buttonLogin.Name = "buttonLogin";
-            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.Size = new System.Drawing.Size(186, 33);
             this.buttonLogin.TabIndex = 4;
             this.buttonLogin.Text = "ログイン";
             this.buttonLogin.UseVisualStyleBackColor = true;
@@ -88,9 +95,10 @@
             // 
             // buttonGetContacts
             // 
-            this.buttonGetContacts.Location = new System.Drawing.Point(14, 70);
+            this.buttonGetContacts.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonGetContacts.Location = new System.Drawing.Point(16, 106);
             this.buttonGetContacts.Name = "buttonGetContacts";
-            this.buttonGetContacts.Size = new System.Drawing.Size(138, 23);
+            this.buttonGetContacts.Size = new System.Drawing.Size(259, 36);
             this.buttonGetContacts.TabIndex = 5;
             this.buttonGetContacts.Text = "取引先責任者を取得";
             this.buttonGetContacts.UseVisualStyleBackColor = true;
@@ -98,9 +106,10 @@
             // 
             // buttonLogout
             // 
-            this.buttonLogout.Location = new System.Drawing.Point(398, 29);
+            this.buttonLogout.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonLogout.Location = new System.Drawing.Point(605, 106);
             this.buttonLogout.Name = "buttonLogout";
-            this.buttonLogout.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogout.Size = new System.Drawing.Size(186, 33);
             this.buttonLogout.TabIndex = 6;
             this.buttonLogout.Text = "ログアウト";
             this.buttonLogout.UseVisualStyleBackColor = true;
@@ -108,16 +117,32 @@
             // 
             // dataGridViewContacts
             // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewContacts.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridViewContacts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridViewContacts.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.FirstName,
             this.LastName,
             this.Email,
             this.Phone});
-            this.dataGridViewContacts.Location = new System.Drawing.Point(14, 99);
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewContacts.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridViewContacts.Location = new System.Drawing.Point(16, 148);
             this.dataGridViewContacts.Name = "dataGridViewContacts";
-            this.dataGridViewContacts.RowTemplate.Height = 21;
-            this.dataGridViewContacts.Size = new System.Drawing.Size(462, 339);
+            this.dataGridViewContacts.RowTemplate.Height = 36;
+            this.dataGridViewContacts.Size = new System.Drawing.Size(776, 406);
             this.dataGridViewContacts.TabIndex = 7;
             // 
             // FirstName
@@ -125,30 +150,34 @@
             this.FirstName.DataPropertyName = "FirstName";
             this.FirstName.HeaderText = "FirstName";
             this.FirstName.Name = "FirstName";
+            this.FirstName.Width = 200;
             // 
             // LastName
             // 
             this.LastName.DataPropertyName = "LastName";
             this.LastName.HeaderText = "LastName";
             this.LastName.Name = "LastName";
+            this.LastName.Width = 200;
             // 
             // Email
             // 
             this.Email.DataPropertyName = "Email";
             this.Email.HeaderText = "Email";
             this.Email.Name = "Email";
+            this.Email.Width = 300;
             // 
             // Phone
             // 
             this.Phone.DataPropertyName = "Phone";
             this.Phone.HeaderText = "Phone";
             this.Phone.Name = "Phone";
+            this.Phone.Width = 200;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(485, 450);
+            this.ClientSize = new System.Drawing.Size(804, 566);
             this.Controls.Add(this.dataGridViewContacts);
             this.Controls.Add(this.buttonLogout);
             this.Controls.Add(this.buttonGetContacts);
